42024f5a7537df194577568f82d61e8fcc137082
slswk59 <120429305+slswk59@users.noreply.github.com>
2023-04-03 14:30:30 +0900

    Delete myapp_frontend_todolist/src directory

--- myapp_frontend_todolist/src/apiurl.js
+++
@@ -1,6 +0,0 @@
-//백과 프론트의 작업을 따로 할때 localhost 자리에 현재 개발자의 ip주소를 넣는다.
-const baseUrl = 'http://localhost:8090';
-//const baseUrl2 = 'http://localhost:8070';
-
-export { baseUrl };
-//export { baseUrl2 };
